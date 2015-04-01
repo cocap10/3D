315060fe42328806109f4f53101c92d73173cf2d
cocap10 <martin.piegay@telecom-st-etienne.fr>
2015-04-01 16:01:50 +0200

    Une belle sphere sans bug :-)

---
+++ myglwidget.cpp
@@ -0,0 +1,217 @@
+// myglwidget.cpp
+
+#include <QtWidgets>
+#include <QtOpenGL>
+
+#include "myglwidget.h"
+
+#include <GL/glu.h>
+
+MyGLWidget::MyGLWidget(QWidget *parent)
+    : QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
+{
+    xRot = 0;
+    yRot = 0;
+    zRot = 0;
+}
+
+MyGLWidget::~MyGLWidget()
+{
+}
+
+QSize MyGLWidget::minimumSizeHint() const
+{
+    return QSize(50, 50);
+}
+
+QSize MyGLWidget::sizeHint() const
+{
+    return QSize(400, 400);
+}
+
+static void qNormalizeAngle(int &angle)
+{
+
+    while (angle < 0)
+        angle += 360;
+    while (angle > 360)
+        angle -= 360;
+    //qDebug()<<angle;
+}
+
+void MyGLWidget::setXRotation(int angle)
+{
+    qNormalizeAngle(angle);
+    if (angle != xRot) {
+        xRot = angle;
+        emit xRotationChanged(angle);
+        updateGL();
+    }
+}
+
+void MyGLWidget::setYRotation(int angle)
+{
+    qNormalizeAngle(angle);
+    if (angle != yRot) {
+        yRot = angle;
+        emit yRotationChanged(angle);
+        updateGL();
+    }
+}
+
+void MyGLWidget::setZRotation(int angle)
+{
+    qNormalizeAngle(angle);
+    if (angle != zRot) {
+        zRot = angle;
+        emit zRotationChanged(angle);
+        updateGL();
+    }
+}
+GLuint loadtgadisplayCDV ( const char* filename )
+{
+    FILE* fp;
+    char magic[12];
+    unsigned char header[6];
+    int imageSize;
+    GLenum type;
+    char temp;
+    int i;
+    int bpp,width,height;
+    GLuint textureId;
+    int globalWidth = 1;
+    int globalHeight = 1;
+    char* imageData;
+    char* imageTmp;
+    if(!(fp = fopen(filename, "rb"))) return 0;
+    if(!fread(&magic, 12, 1, fp)) return 0;
+    if(!fread(&header, 6, 1, fp)) return 0;
+    bpp = header[4];
+    width  = header[1] * 256 + header[0];
+    height = header[3] * 256 + header[2];
+    if(width <= 0 || height <= 0) return 0;
+    if(bpp != 24 && bpp != 32) return 0;
+    imageSize = width * height * (bpp / 8);
+    imageData = (char *)malloc(sizeof(char) * imageSize);
+    if(imageData == NULL) return 0;
+    if(!fread(imageData, imageSize, 1, fp)) { free(imageData); return 0; }
+    for(i = 0; i < imageSize; i += bpp / 8) {
+        temp = imageData[i];
+        imageData[i] = imageData[i + 2];
+        imageData[i + 2] = temp; }
+    if(bpp == 32) for(i=0;i<imageSize;i+=4) imageData[i+3]=255-imageData[i+3];
+    fclose(fp);
+    while (width>globalWidth) globalWidth*=2;
+    while (height>globalHeight) globalHeight*=2;
+    glEnable( GL_TEXTURE_2D );
+    glGenTextures(1, &textureId);
+    glBindTexture(GL_TEXTURE_2D, textureId);
+    glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glPixelStorei(GL_UNPACK_ROW_LENGTH,globalWidth);
+    glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+    if(bpp == 24) type=GL_RGB; else type=GL_RGBA;
+    imageTmp = (char *)malloc(sizeof(char)*globalWidth*globalHeight*4);
+    if (imageTmp==NULL) return 0;
+    glTexImage2D(GL_TEXTURE_2D,0,type,globalWidth,globalHeight,0,
+                 type,GL_UNSIGNED_BYTE,imageTmp);
+    free (imageTmp );
+    glPixelStorei(GL_UNPACK_ROW_LENGTH,width);
+    glTexSubImage2D(GL_TEXTURE_2D,0, 0,0, width, height,
+                    type ,GL_UNSIGNED_BYTE, imageData );
+    free(imageData);
+    return textureId;
+}
+
+
+
+void MyGLWidget::initializeGL()
+{
+    qglClearColor(Qt::blue);//couleur de fond
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glDisable(GL_CULL_FACE);
+    glEnable ( GL_NORMALIZE );
+    glDepthMask ( GL_TRUE );
+    glBlendFunc(GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA);
+    glPointSize ( 1.0f );
+    glLineWidth ( 1.0f );
+    glEnable(GL_COLOR_MATERIAL);
+    GLtexture[0]= loadtgadisplayCDV( "./BDS.tga");
+    qDebug()<<GLtexture[0];
+
+}
+
+void MyGLWidget::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    //Init le system de coordonnee
+    glRotatef(90.0,1.0,0.0,0.0);
+    glRotatef(90.0,0.0,0.0,1.0);
+
+    //Realiser les transfo du monde
+    glTranslatef(-10.0, 0, 0);
+
+    glRotatef(-xRot , 0.0, 1.0, 0.0);//theta
+    glRotatef(-yRot , 0.0, 0.0, 1.0);//phi
+    glRotatef(-zRot , 1.0, 0.0, 0.0);
+
+    /*    glTranslatef(0.0, 0.0, -10.0);
+    glRotatef(xRot / 16.0, 1.0, 0.0, 0.0);
+    glRotatef(yRot / 16.0, 0.0, 1.0, 0.0);
+    glRotatef(zRot / 16.0, 0.0, 0.0, 1.0);*/
+    draw();
+}
+
+void MyGLWidget::resizeGL(int width, int height)//propriete camera
+{
+    int side = qMin(width, height);
+    glViewport((width - side) / 2, (height - side) / 2, side, side);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    /*#ifdef QT_OPENGL_ES_1
+    glOrthof(-2, +2, -2, +2, 1.0, 15.0);
+#else
+    glOrtho(-2, +2, -2, +2, 1.0, 15.0);
+#endif*/
+    gluPerspective(70,((float)width/(float)height),0.01,30);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void MyGLWidget::mousePressEvent(QMouseEvent *event)
+{
+    lastPos = event->pos();
+}
+
+void MyGLWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    int dx = event->x() - lastPos.x();
+    int dy = event->y() - lastPos.y();
+
+    if (event->buttons() & Qt::LeftButton) {
+        setXRotation(xRot + dy);
+        setYRotation(yRot + dx);
+    } else if (event->buttons() & Qt::RightButton) {
+        setXRotation(xRot + dy);
+        setZRotation(zRot + dx);
+    }
+
+    lastPos = event->pos();
+}
+
+void MyGLWidget::draw()
+{
+    quadric = gluNewQuadric();
+    gluQuadricDrawStyle(quadric, GLU_FILL );
+        gluSphere( quadric , 4, 10 , 10 );
+        /*glPushMatrix();
+        glTranslatef(5,5,5);
+        gluCylinder(quadric,2,2,4,10,10);
+        glPopMatrix();*/
+        gluDeleteQuadric(quadric);
+}
---
+++ myglwidget.h
@@ -0,0 +1,63 @@
+// myglwidget.h
+
+#ifndef MYGLWIDGET_H
+#define MYGLWIDGET_H
+
+#include <QGLWidget>
+#include <GL/glu.h>
+
+class MyGLWidget : public QGLWidget
+{
+    Q_OBJECT
+public:
+    explicit MyGLWidget(QWidget *parent = 0);
+    ~MyGLWidget();
+signals:
+
+public slots:
+
+protected:
+    void initializeGL();
+    void paintGL();
+    void resizeGL(int width, int height);
+
+    QSize minimumSizeHint() const;
+    QSize sizeHint() const;
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+
+
+public slots:
+    // slots for xyz-rotation slider
+    void setXRotation(int angle);
+    void setYRotation(int angle);
+    void setZRotation(int angle);
+
+signals:
+    // signaling rotation from mouse movement
+    void xRotationChanged(int angle);
+    void yRotationChanged(int angle);
+    void zRotationChanged(int angle);
+
+private:
+    void draw();
+
+    int xRot;
+    int yRot;
+    int zRot;
+
+    QPoint lastPos;
+
+    /*
+     * texture
+     */
+    GLuint loadtga ( const char* filename );
+    GLfloat GLfloatData[4];
+    GLdouble GLdoubleData[4];
+    GLuint GLtexture[1];
+
+    GLUquadricObj *quadric;
+};
+GLuint loadtgadisplayCDV ( const char* filename );//textures
+#endif // MYGLWIDGET_H
+
