c8b9d0fba42d743a6d07b30e68746f4a4c8d1728
share70 <yvann-du-46@hotmail.fr>
2015-05-20 15:37:28 +0200

    oublie d'inclure  arene.cpp au dernier commit

--- arene.cpp
+++ arene.cpp
@@ -10,6 +10,12 @@ Arene::~Arene()
     qDebug()<<"Fin arene";
 }
 
+void Arene::init()
+{
+    //Texture du sol
+    GLtexture= loadtgadisplayCDV("BDS.tga");
+}
+
 void Arene::draw(float rA, float oT, float dT, float rT, int p)
 {
     rArenne=rA;
@@ -21,6 +27,21 @@ void Arene::draw(float rA, float oT, float dT, float rT, int p)
     glColor3f(1,1,0);
     std::vector<Point> pointsTrou = calculerPointsCercles(oTrou, dTrou, rTrou, pas);
     std::vector<Point> pointsCercle = calculerPointsCercles(0,0,rArenne,pas);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glDisable(GL_CULL_FACE);
+    glEnable ( GL_NORMALIZE );
+    glDepthMask ( GL_TRUE );
+    glBlendFunc(GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA);
+    glPointSize ( 1.0f );
+    glLineWidth ( 1.0f );
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable( GL_DEPTH_TEST );
+    glEnable( GL_CULL_FACE );
+    glColor3f(1,1,1);
+    glEnable( GL_TEXTURE_2D );
+    glDisable( GL_CULL_FACE );
+    glBindTexture(GL_TEXTURE_2D, GLtexture);
     glBegin(GL_TRIANGLE_STRIP);
     //Boucle de construction
     for(int i=0; (i<pointsTrou.size() || i<pointsCercle.size()); i++)
@@ -28,12 +49,19 @@ void Arene::draw(float rA, float oT, float dT, float rT, int p)
 
         Point pointCercle=pointsCercle[i];
         Point pointTrou=pointsTrou[i];
+
+        glTexCoord2f((1+pointCercle.getX())/2, (1+pointCercle.getY())/2);
         glVertex3f(pointCercle.getX(),pointCercle.getY(),pointCercle.getZ());
+        //glTexCoord2f((1+pointCercle.getX()/rArenne)/2,);
+        glTexCoord2f((1+pointTrou.getX())/2, (1+pointTrou.getY())/2);
         glVertex3f(pointTrou.getX(),pointTrou.getY(),pointTrou.getZ());
-        /*qDebug()<<"p1(x,y): "<<pointCercle.getX()<<" "<< pointCercle.getY()
-                <<",p2(x,y): "<<pointTrou.getX() <<" "<<pointTrou.getY();*/
     }
     glEnd();
+    glDisable( GL_TEXTURE_2D );
+    glEnable( GL_CULL_FACE );
+
+
+    glDisable( GL_CULL_FACE );
     glBegin(GL_QUAD_STRIP);
     for(int i=0;  i<pointsCercle.size(); i++)
     {
@@ -42,6 +70,7 @@ void Arene::draw(float rA, float oT, float dT, float rT, int p)
         glVertex3f(pointCercle.getX(),pointCercle.getY(),pointCercle.getZ());
     }
     glEnd();
+    glEnable( GL_CULL_FACE );
 
 
 
